a258b0a162a24a27b76839555b3189bf3373e9c5
jmittere <justin.mittereder@gmail.com>
2024-11-13 23:01:50 -0500

    adding events for choosing winner and adding to vote method logic

--- voting_system/tests/test_ballot.py
+++ voting_system/tests/test_ballot.py
@@ -12,3 +12,8 @@ def vote():
 def test_candidate(vote):
     candidate = vote.create_candidate("John Doe", '0x66aB6D9362d4F35596279692F0251Db635165871', 'Virginia', 30, {'from': accounts[0], 'priority_fee': 10, 'max_fee': 1000000000})
     assert candidate.events['CandidateCreated'].values() == ["John Doe", '0x1CEE82EEd89Bd5Be5bf2507a92a755dcF1D8e8dc', 'Virginia', 30]
+
+#test vp candidate
+def test_vp_candidate(vote):
+    candidate = vote.create_candidate("Jane Smith", '0x66aB6D9362d4F35596279692F0251Db635165871', 'Texas', 35, {'from': accounts[0], 'priority_fee': 10, 'max_fee': 1000000000})
+    assert candidate.events['CandidateCreated'].values() == ["John Doe", '0x1CEE82EEd89Bd5Be5bf2507a92a755dcF1D8e8dc', 'Virginia', 30]
